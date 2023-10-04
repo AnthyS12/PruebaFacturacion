7308c43b811a67d6cf1229d35a5b3610fda5d1bf
AnthyS12 <anthonymoreira768@gmail.com>
2023-10-03 23:36:04 -0500

    con

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'admin');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'sisfactura');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'SoftJACode');
+define('DIRECCION_EMPRESA', 'LIMA PERÚ');
+define('TELEFONO_EMPRESA', '+(51) 931499114');
+define('EMAIL_EMPRESA', 'ARANGO@GMAIL.COM');
+define('TAX', '11');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
